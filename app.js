b23133301d92686562dc53a8abedeb6620cfb141
dmcclendon56 <mcclendon.darrian@gmail.com>
2022-04-12 11:31:57 -0500

    shuffle function draft 1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const square = document.querySelectorAll('.slot')
 const item1 = document.getElementById('slot1')
 const item2 = document.getElementById('slot2')
 const item3 = document.getElementById('slot3')
+const winningDiv = document.querySelector('.winner')
 //images in the array
 let cherryHTML = "<img src=\"https://cdn3.iconfinder.com/data/icons/slot-machine-symbols-filled-outline/256/cherry-512.png\" width=\"150px\" height=\"150px\">"
 let sevenHTML = "<img src=\"https://cdn3.iconfinder.com/data/icons/slot-machine-symbols-filled-outline/256/7-512.png\" width=\"150px\" height=\"150px\">"
@@ -18,7 +19,26 @@ let shoeHTML = "<img src=\"https://cdn3.iconfinder.com/data/icons/slot-machine-s
 
 //array of images to be shuffled through
 const slot1 = [cherryHTML, sevenHTML, barHTML, bellHTML, diamondHTML, cloveHTML, shoeHTML];
-// const shuffleArray = array.sort((a,b) => 0.5 - Math.random());
+
+let j = 1;
+//shuffle pictures 'how many' times
+const delay = 10;
+//50 miliseconds between each cycle
+const speed = 50;
+//stackoverflow- http://jsfiddle.net/Curt/rjve4whe/1/ 
+function shufflePicture() {
+    console.log('shuffle picture')
+    setTimeout(function () {
+        console.log('timeout function')
+        square.forEach(square => {
+            square.innerHTML = getRandomNumber()
+        })
+        j++;
+        if (j < delay) {
+            shufflePicture();
+        }
+    }, speed)
+};
 
 //array to call on for shuffle function
 function getRandomNumber () {
@@ -28,18 +48,18 @@ function getRandomNumber () {
 
 //Spin button 
 lever.addEventListener('click', () => {
-    square.forEach(square => {
-        square.innerHTML = getRandomNumber()
-        // square.innerHTML = imgHTML
-    })
-//win statement
+    j = 1;
+    shufflePicture();
+    setTimeout(function () {
+        //win statement
         if(item1.innerHTML == item2.innerHTML && item2.innerHTML == item3.innerHTML){
+            winningDiv.innerHTML = 'WINNER!'
             console.log('You win!')
-    }else{
-        console.log('You lose')
-    }
-    
-
+            }else{
+            console.log('You lose')
+        }
+    }, delay * (speed +100))
+ 
 })
 //display "winner"/Loser
 
